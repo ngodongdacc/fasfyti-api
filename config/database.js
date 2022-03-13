7bfee4048ad4acd0c6a06821b0e35696a4f30b42
ngodongdacc <ngodongdac3005@gmail.com>
2022-03-13 17:04:29 +0700

    .env dabase

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const database = require('knex')({
     client: 'mysql2',
     connection: {
-        host: '127.0.0.1', // process.env.DB_HOST,
-        port: 3306, //  process.env.DB_PORT,
-        user: 'dongngo', // process.env.DB_USER,
-        password: 'dong1234567890', // process.env.DB_PASSWORD,
-        database: 'test', // process.env.DB_NAME
+        host: process.env.DB_HOST,
+        port:  process.env.DB_PORT,
+        user:  process.env.DB_USER,
+        password:  process.env.DB_PASSWORD,
+        database:  process.env.DB_NAME
     },
     // useNullAsDefault: true
 });
